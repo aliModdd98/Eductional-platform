27922646d384fe5d475f8cc313e25c26c6e29cc8
aliMohamad98 <eng.alimoh98m@gmail.com>
2024-10-14 10:37:01 +0300

    update deploy 1

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import react from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base:"/Eductional-platform",
   plugins: [react()],
-})
+});
