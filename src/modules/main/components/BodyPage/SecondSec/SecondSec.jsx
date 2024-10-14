dbae8185b80d45bf3e08ea637267d22643ac692d
aliMohamad98 <eng.alimoh98m@gmail.com>
2024-10-14 11:17:23 +0300

    fixes deploy vercel

--- src/modules/main/components/BodyPage/SecondSec/SecondSec.jsx
+++ src/modules/main/components/BodyPage/SecondSec/SecondSec.jsx
@@ -1,19 +1,17 @@
-
-import img from './../../../../../assets/girl.webp'
-import './SecondSecStyle.CSS'
-import Description from './Description.JSX'
+import img from "./../../../../../assets/girl.webp";
+import "./SecondSecStyle.css";
+import Description from "./Description.JSX";
 function SecondSec() {
-const title2="Become an Instructor"; 
+  const title2 = "Become an Instructor";
   return (
-    <div className='sec-container'>
-   
-  <Description title={title2}/>
-  <button className='btn1'>GET STARTED</button>
-<div className="right-sec">
-  <img src={img}/>
-</div>
+    <div className="sec-container">
+      <Description title={title2} />
+      <button className="btn1">GET STARTED</button>
+      <div className="right-sec">
+        <img src={img} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default SecondSec
+export default SecondSec;
