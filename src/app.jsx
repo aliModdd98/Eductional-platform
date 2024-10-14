9cc6f29c5a4321fc92a850d498b8c077fc528eb4
aliMohamad98 <eng.alimoh98m@gmail.com>
2024-10-14 11:07:55 +0300

    fixes deploy gh1

--- src/app.jsx
+++ src/app.jsx
@@ -1,32 +1,24 @@
-import 'react-router-dom'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import LandingPage from './components/Home/LandingPage';
-import NavigateTo from './router/Navigate';
-import { Provider } from 'react-redux';
+import "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./app.css";
+import LandingPage from "./components/Home/LandingPage";
+import NavigateTo from "./router/Navigate";
+import { Provider } from "react-redux";
 // import Loader from './components/Loader/Loader';
-import store from './store/index'
-
+import store from "./store/index";
 
 function App() {
- 
-
   // const { isLoading, teachers, courses, students } = useDataFetching();
   return (
- <Provider store={store}>
-    <Router>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="*" element={<NavigateTo />} />
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="*" element={<NavigateTo />} />
         </Routes>
-  </Router>
-  </Provider>
-  )}
-       
+      </Router>
+    </Provider>
+  );
+}
 
-
-
-     
-
-
-export default App
+export default App;
